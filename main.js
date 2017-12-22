4e4d22ff53d1a3c0a14f82305cb326e1a31709c1
svivek71@yahoo.com <svivek71@yahoo.com>
2017-12-21 21:48:21 -0800

    sort error handling

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ let map, infoWindow;
 let pos = {};
 let des = [];
 let elevPos = {};
+let statusE = false;
+let statusD = false;
 
 let searchHistory = [];
 let searchResults = [];
@@ -20,7 +22,8 @@ function SearchResultsObject(name, add, openh, dis, ele, rating, elecomp, imgUrl
   this.imgUrl = imgUrl;
   this.equivdist = ed;
 }
-// this.distance + (7.92*this.elecomp)
+// this.equivdist = this.distance + (7.92*this.elecomp)
+
 function initMap(e) {
   e.preventDefault();
 
@@ -36,6 +39,7 @@ function initMap(e) {
         };
         // empty the handlebars and results
         searchResults = [];
+        des = [];
         $('.search-details').empty();
 
         var elevator = new google.maps.ElevationService;
@@ -53,8 +57,8 @@ function initMap(e) {
 
         let request = {
           location: pos,
-          // rankBy: google.maps.places.RankBy.DISTANCE,
-          radius: '500',
+          //rankBy: google.maps.places.RankBy.DISTANCE,
+          radius: '5000', // in meters
           // name: [$('#search-name').val()],//search by name
           // type: [$('#search-type').val()],// search by type
           keyword: [$('#search').val()]// search by keyword
@@ -91,15 +95,26 @@ function processResults(results, status) {
       searchResults[i].imgUrl = (results[i].photos) ? results[i].photos[0].getUrl({maxWidth: 1000}) : 'img/notfound.jpg';
       searchResults[i].openhrs = (results[i].opening_hours) ? results[i].opening_hours : 'Not Available';
     }
-    // console.log(results);
+    //  console.log('results', results);   
   }
   let distance = new google.maps.DistanceMatrixService;
-  let statusD = distanceLocation(distance);
+  statusD = distanceLocation(distance);
   let elevator = new google.maps.ElevationService;
-  let statusE = displayLocationElevation(elevator);
+  statusE = displayLocationElevation(elevator);
 
-  setTimeout(equivdistCalc, 1000);
+
+  setTimeout(equivdistCalc, 2000);
+  
+  // let timeVar = setInterval(function() {
+  //   console.log('D-', statusD, 'E-',statusE);
+  //   if (statusD && statusE) {
+  //       clearInterval(timeVar);
+  //       equivdistCalc();
+  //   }
+  // },1000);
+
 }
+
 
 function centerMarker() {
   let marker = new google.maps.Marker({
@@ -125,7 +140,6 @@ function createMarker(place) {
 
 //calculate distance
 function distanceLocation(distance) {
-  let statusD = false;
   for (let i = 0; i < searchResults.length; i++) {
     distance.getDistanceMatrix({
       origins: [pos],
@@ -134,7 +148,7 @@ function distanceLocation(distance) {
       unitSystem: google.maps.UnitSystem.IMPERIAL,
     }, function(results, err){
       searchResults[i].distance =  Number((results.rows[0].elements[0].distance.text).substr(0,(results.rows[0].elements[0].distance.text).length-3));
-      statusD = true;
+      if (i == searchResults.length) {statusD = true;} 
     })
   }
   return statusD;
@@ -142,14 +156,13 @@ function distanceLocation(distance) {
 
 // calculate elevation
 function displayLocationElevation(elevator) {
-  let statusE = false;
   for (let i = 0; i < searchResults.length; i++) {
     elevator.getElevationForLocations({
       locations: [des[i]],
     }, function(response, err){
       searchResults[i].elevation =  Math.floor(response[0].elevation*3.28);
       searchResults[i].elevationcomp =  Math.abs(searchResults[i].elevation - elevPos);
-      statusE = true;
+      if (i == searchResults.length) {statusE = true;} 
     });
   }
   return statusE;
@@ -160,12 +173,12 @@ function equivdistCalc() {
     let naismith_ed = ((((searchResults[i].distance*1.6) + (7.92*(searchResults[i].elevationcomp*.3048/1000))))*0.62);
     searchResults[i].equivdist = Number(naismith_ed.toPrecision(2));
   }
-  searchResults.sort((a, b) => {
-    return a.equivdist - b.equivdist;
-  })
+  searchResults.sort((a, b) => {a.equivdist - b.equivdist});
+  
+  console.log(searchResults);
   setTimeout(accordPopulate, 1000);
   checkSearchResultIsNone();
-  historyLog();
+  // historyLog();
 }
 // this functions tell you if you are allowed the GPS to be accessed.
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
@@ -174,7 +187,6 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     'Error: The Geolocation service failed.' :
     'Error: Your browser doesn\'t support geolocation.');
   infoWindow.open(map);
-
 }
 
 function checkSearchResultIsNone(){
@@ -183,9 +195,9 @@ function checkSearchResultIsNone(){
   }
 }
 
-function historyLog(){
-  let now = Date().split(' ').slice(0, 4).join(' ');
-  searchHistory.forEach(search => {
-    console.log(`${now}: ${search}`)
-  });
-}
+// function historyLog(){
+//   let now = Date().split(' ').slice(0, 4).join(' ');
+//   searchHistory.forEach(search => {
+//     console.log(`${now}: ${search}`)
+//   });
+// }
